3948c12fb3095fc96d3763e7a0ca77a60ac49c7f
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-01-13 20:44:12 -0700

    Replaced files to original from Lambda.  Did this project in a previous cohort, reverting back to start from scratch.

--- src/App.js
+++ src/App.js
@@ -1,82 +1,13 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
-import TodoForm from './components/TodoComponents/TodoForm';
-import TodoList from './components/TodoComponents/TodoList';
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todoList: [
-        {
-        task: 'Organize Garage',
-        id: '1528817077286',
-        completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: '1528817084358',
-          completed: false
-        },
-      ],
-      task: ''
-    };
-  }
-
-  //Event Handlers:
-  changeHandler = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  addTodo = event => {
-    event.preventDefault();
-    let newTask = {
-      task: this.state.task,
-      id: Date.now(),
-      completed: false
-    };
-    this.setState({
-      todoList: [...this.state.todoList, newTask],
-      task: ''
-      });
-  };
-
-  //For strikethrough styling:
-  handleClickCompleted = event => {
-    if (event.target.className !== 'completed') {
-      event.target.className = 'completed';
-    }
-    else {
-      event.target.className = 'newTask';
-    }
-  };
-
-  clearCompleted = event => {
-    event.preventDefault();
-    const completedTasks = document.querySelectorAll('.completed');
-    const taskList = document.querySelector('.todoList');
-    completedTasks.forEach(task => taskList.removeChild(task));
-  }
-
+class App extends Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
-    return(
-      <div className="app-container">
-        <h1>Todo List: MVP</h1>
-        <div className="todoList-container">
-          <TodoList 
-            list={this.state.todoList} 
-            clickCompleted={this.handleClickCompleted}
-          />
-        </div>
-        <div className="todoForm-container">
-          <TodoForm 
-            addTodo={this.addTodo}
-            changeHandler={this.changeHandler}
-            task={this.state.task}
-            complete={this.clearCompleted}
-          />
-        </div>
+    return (
+      <div>
+        <h2>Welcome to your Todo App!</h2>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,18 +1,2 @@
-import React from 'react';
-
-import Todo from './Todo';
-
-function TodoList(props) {
-  return (
-    <ul className='todoList'>
-      {props.list.map(listItem => (
-        <Todo 
-          item={listItem}
-          clickCompleted={props.clickCompleted}
-        />
-      ))}
-    </ul>
-  );
-}
-
-export default TodoList;
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
