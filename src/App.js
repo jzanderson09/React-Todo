0eb7b5e70e1bc9cc215aa5d47acb7cc938dd5087
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-01-13 20:48:41 -0700

    Initial push -- restarting components over, commented out import on App and setting up each file, starting from scratch.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+// import TodoList from './components/TodoList';
+// import Todo from './components/Todo';
+// import TodoForm from './components/TodoForm';
+
 class App extends Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div>
